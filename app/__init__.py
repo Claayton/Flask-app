d4d146d32af5e488d0ac397d4d6b1a186b7b629b
Clayton Garcia <claaytongarcia@gmail.com>
2021-08-07 12:28:39 -0300

    Um pouco sobre rotas

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask_migrate import Migrate, MigrateCommand
 # O MigrateComand só esta funcionando até a versão 'Flask-Migrate==2.6.0'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config.from_object('config')
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,13 @@
 from app import app
 
 
+@app.route('/index')
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello World!!'
+
+
+
+@app.route('/test/', methods=['GET'])
+def teste():
+    return f'Olá, Marilene!!'
