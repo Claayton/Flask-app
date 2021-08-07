a21223184fdb6f8ef2217a89bab2834748bebd72
Clayton Garcia <claaytongarcia@gmail.com>
2021-08-07 11:45:00 -0300

    Configurando db e arquivos de migraçao

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+# O MigrateComand só esta funcionando até a versão 'Flask-Migrate==2.6.0'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
