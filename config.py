c88961399849db31d57dcad49d3df25398b6a202
Clayton Garcia <claaytongarcia@gmail.com>
2021-10-25 13:40:40 -0300

    mudado o db para sqlite

--- config.py
+++ config.py
@@ -6,10 +6,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('secret_key')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}@{}/mitmirror".format(
-    os.getenv('mysql_username'),
-    os.getenv('mysql_password'),
-    os.getenv('mysql_server'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.sqlite3'
 
 email_mitmirrortests = os.getenv('email_mitmirrortests')
 password_mitmirrortests = os.getenv('password_mitmirrortests')
